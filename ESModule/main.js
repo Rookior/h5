860da9db4d737aba769c80ea95e3a6c6661dfd42
rookior <1757629766@qq.com>
2022-08-12 11:10:35 +0800

    export default 需要用default接收

--- ESModule/main.js
+++ ESModule/main.js
@@ -1,5 +1,9 @@
 import { name, age } from "./moduleA.js";
 console.log(name, age);
 import * as moduleA from "./moduleA.js";
-console.log(moduleA)
+console.log(moduleA,moduleA.name,moduleA.age)
 moduleA.sayHello('world')
+
+import * as moduleB from "./moduleB.js";
+console.log(moduleB.default.sex)
+
---
+++ ESModule/moduleB.js
@@ -0,0 +1,3 @@
+export default {
+    sex:'female'
+}
